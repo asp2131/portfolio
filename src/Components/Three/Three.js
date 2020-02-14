4631336fb0ebc2d9199c72d448ba7c93bf37931b
asp2131 <akinpds@gmail.com>
2020-02-13 22:53:16 -0600

    info posted with art

--- src/Components/Three/Three.js
+++ src/Components/Three/Three.js
@@ -6,6 +6,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import Radio from "@material-ui/core/Radio";
 import Paper from "@material-ui/core/Paper";
+import color from "@material-ui/core/colors/amber";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,12 +37,58 @@ export default function SpacingGrid() {
             <Grid item>
               <Paper className={classes.paper} >
                 <img src={require('../../tokyo.jpg')} style={{width: 310, height: 310}}/>
+                <p style={
+                          {
+                          display: 'flex', 
+                          justifyContent: 'center',
+                          fontWeight: 'bold', 
+                          fontSize: '20x', 
+                          marginBottom: '4px',
+                          marginTop: '6px'
+                          }}>{`tune. - TOKYO!`}</p>
+                <p style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          fontWeight: 'bold',
+                          fontSize: '20x',
+                          marginBottom: '16px'
+                        }}>Produced, Mixed, Wrote</p>
+              <a href="https://open.spotify.com/track/5MCOgFwTZ0YovAtr1UwV6q"
+                style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          fontWeight: 'bold',
+                          fontSize: '20x',
+                          marginBottom: '16px'
+                        }}>I produced this gem, listen here</a>
               </Paper>
             </Grid>
           
           <Grid item>
             <Paper className={classes.paper} >
               <img src={require('../../TUNE_COVERART.jpg')} style={{ width: 310, height: 310 }} />
+              <p style={{
+                display: 'flex',
+                justifyContent: 'center',
+                fontWeight: 'bold',
+                fontSize: '20x',
+                marginBottom: '4px',
+                marginTop: '6px'
+              }}>{`tune. - Come Vibe`}</p>
+              <p style={{
+                display: 'flex',
+                justifyContent: 'center',
+                fontWeight: 'bold',
+                fontSize: '20x',
+                marginBottom: '16px'
+              }}>Produced, Mixed, Wrote</p>
+              <p style={{
+                display: 'flex',
+                justifyContent: 'center',
+                fontWeight: 'bold',
+                fontSize: '24x',
+                marginBottom: '16px'
+              }}>Coming Soon!</p>
             </Paper>
           </Grid>
 
