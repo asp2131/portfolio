4309e7457064589b6daf4b94f13a94d24e70c30c
asp2131 <akinpds@gmail.com>
2020-02-16 16:58:20 -0600

    music player renders msusic

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Playlist from 'react-mp3-player';
 
 export default function AudioPlayer(){
-    const tracks = [{ img: 'https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg', name: 'MP3', desc: 'Description 1', src: 'https://res.cloudinary.com/https-pilot-tune-herokuapp-com/video/upload/v1581756932/come_vibe_final_vtfuqy.wav' },
-        { img: 'https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg', name: 'MP3 #2', desc: 'Description 2', src: 'https://res.cloudinary.com/https-pilot-tune-herokuapp-com/video/upload/v1581757054/MASTERED_4-tokyo_normalize_mix_2-px24-uhq_z237yc.wav' }]
+    const tracks = [{ img: require('../../assets/TUNE_COVERART.jpg'), name: 'ComeVibe', desc: 'produced by tune.', src: 'https://res.cloudinary.com/https-pilot-tune-herokuapp-com/video/upload/v1581756932/come_vibe_final_vtfuqy.wav' },
+        { img: require('../../assets/tokyo.jpg'), name: 'TOKYO!', desc: 'produced by tune.', src: 'https://res.cloudinary.com/https-pilot-tune-herokuapp-com/video/upload/v1581757054/MASTERED_4-tokyo_normalize_mix_2-px24-uhq_z237yc.wav' }]
 
     const playlistOverideStylingOpts = {
         
